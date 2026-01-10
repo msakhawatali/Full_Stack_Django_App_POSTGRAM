dc59539d4409e3c24aa740a50973c9353506bad2
sakhawatali <msakhawatali131@gmail.com>
2026-01-10 16:21:38 +0500

    adding user profile interface

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class userprofile(models.Model):
+    userr = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.IntegerField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_pic = models.ImageField(upload_to="userprofile", null=True, blank=True)
